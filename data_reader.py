733de95ccfc179546c3b7cadf68b70dc83b2acae
WEN Hao <wenh06@gmail.com>
2024-04-16 11:28:39 +0800

    finish the visualization method for the data reader

--- data_reader.py
+++ data_reader.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from typing import Any, Optional, Union
+from typing import Any, Optional, Sequence, Union
 
 import nibabel as nib
 import numpy as np
@@ -16,7 +16,10 @@ _MBAS2024_INFO = DataBaseInfo(
     MICCAI MBAS 2024 - Multi-class Bi-Atrial Segmentation Challenge
     """,
     about="""
-    1.
+    1. The dataset consists of 3D LGE-MRI scans of the left atrium and right atrium, and their walls.
+    2. The dataset is divided into 3 subsets: training (70 samples), validation (30 samples), and testing (100 samples, hidden).
+    3. The data as well as the segmentation annotations are in NIfTI format.
+    4. The segmentation annotations are multi-class, with 4 classes: right atrium (1), left atrium (2), left & right atrial walls (3), and background (0).
     """,
     usage=[
         "Atrium Segmentation",
@@ -46,6 +49,21 @@ class MBAS2024(_DataBase):
         Auxilliary key word arguments
 
     """
+
+    __class_map__ = {
+        0: "background",
+        1: "right atrium",
+        2: "left atrium",
+        3: "left & right atrial walls",
+    }
+    __label2id__ = {v: k for k, v in __class_map__.items()}
+    __id2label__ = {k: v for k, v in __class_map__.items()}
+    __palette__ = {
+        0: (0, 0, 0, 0),  # "background" (transparent)
+        1: "red",  # "right atrium"
+        2: "blue",  # "left atrium"
+        3: "yellow",  # "left & right atrial walls"
+    }
 
     def __init__(
         self,
@@ -79,6 +97,7 @@ class MBAS2024(_DataBase):
         self._df_records["subset"] = self._df_records["path"].apply(lambda x: x.parents[1].name)
         self._df_records["ann_path"] = self._df_records["path"].apply(lambda x: Path(str(x).replace("gt", "label")))
         self._df_records["shape"] = self._df_records["path"].apply(lambda x: nib.load(str(x)).header.get_data_shape())
+        self._df_records["channels"] = self._df_records["shape"].apply(lambda x: x[-1])
         self._df_records["best_affine"] = self._df_records["path"].apply(lambda x: nib.load(str(x)).header.get_best_affine())
         self._df_records["base_affine"] = self._df_records["path"].apply(lambda x: nib.load(str(x)).header.get_base_affine())
         self._df_records["qform"] = self._df_records["path"].apply(lambda x: nib.load(str(x)).header.get_qform())
@@ -171,22 +190,66 @@ class MBAS2024(_DataBase):
         """
         return nib.load(str(self.get_ann_path(rec))).get_fdata()
 
-    def view_data(self, rec: Union[str, int], orthoview: bool = False) -> None:
+    def view_data(
+        self,
+        rec: Union[str, int],
+        channels: Optional[Union[Sequence[int], int]] = None,
+        with_ann: bool = True,
+        orthoview: bool = False,
+    ) -> None:
         """View the 3D LGE-MRI of a record.
 
         Parameters
         ----------
         rec : str or int
             The record name or index in self._all_records.
+        channels : int or Sequence[int], optional
+            The channel(s) to view. If None, view all channels.
+            Valid only when orthoview is False.
+        with_ann : bool, default True
+            Whether to overlay the segmentation annotation on the MRI.
         orthoview : bool, default False
             Whether to view the MRI in orthogonal view.
 
         """
+        if "plt" not in globals():
+            import matplotlib.pyplot as plt
+            from matplotlib.colors import ListedColormap
         if isinstance(rec, int):
             rec = self._all_records[rec]
+        data = self.load_data(rec)
         if orthoview:
-            raise NotImplementedError
-        raise NotImplementedError
+            data.orthoview()
+            return
+        if with_ann:
+            seg_ann = self.load_ann(rec)
+        else:
+            seg_ann = np.full_like(data, self.__label2id__["background"])
+        if channels is None:
+            channels = list(range(self._df_records.loc[rec, "channels"]))
+        elif isinstance(channels, int):
+            channels = [channels]
+        num_channels = len(channels)
+
+        fig_height = int(np.ceil(num_channels / 4).item()) * 5
+        fig, ax = plt.subplots(fig_height // 5, min(4, num_channels), figsize=(20, fig_height))
+        plt.subplots_adjust(wspace=0.1, hspace=0.1)
+        if num_channels == 1:
+            ax = [ax]
+        else:
+            ax = ax.ravel()
+        seg_cmap = ListedColormap([self.__palette__[cls_] for cls_ in self.__class_map__ if cls_ in self.__palette__])
+        for ax_idx, chan_idx in enumerate(channels):
+            ax[ax_idx].set_axis_off()
+            ax[ax_idx].imshow(data[..., chan_idx], cmap="gray")
+            ax[ax_idx].set_title(f"Channel {chan_idx}")
+            # add annotation
+            chan_ann = seg_ann[..., chan_idx]
+            # for cls_ in np.unique(chan_ann):
+            #     if cls_ == self.__label2id__["background"]:
+            #         continue
+            #     ax[ax_idx].contour(chan_ann == cls_, colors=self.__palette__[cls_], linewidths=1, hatches=["//"])
+            ax[ax_idx].imshow(chan_ann, cmap=seg_cmap, alpha=0.1)
 
     @property
     def database_info(self) -> DataBaseInfo:
